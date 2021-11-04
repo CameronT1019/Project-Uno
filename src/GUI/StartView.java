d85fd7e9ed84dfc143a700c4e9743659cce5f33a
Cameron Testerman <cameron.testerman@outlook.com>
2021-11-04 18:11:48 -0500

    Fixed dispose issue

--- src/GUI/StartView.java
+++ src/GUI/StartView.java
@@ -1,90 +1,91 @@
 
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class StartView extends JPanel implements ActionListener {
-    GridBagConstraints constraints = new GridBagConstraints();
-
-    private ImageIcon uno_image;
-    private JButton quit;
-    private JButton playWithFriends;
+/**
+ * The StartView class which is the first JFrame to begin the Uno Game.
+ */
+public class StartView extends JFrame implements ActionListener {
+    /**
+     * The JFrame Components that are necessary for the StartView class.
+     */
+    private JPanel menu;
     private JLabel label1;
-    static JFrame f = new JFrame("UNO");
-    JFrame frame1;
+    private JLabel name;
+    private ImageIcon uno_image;
+    private JButton playWithPlayer;
+    private JButton quit;
+    private JButton playWithBots;
 
+    /**
+     * The Constructor for the StartView class
+     */
     public StartView() {
-        setLayout(new GridBagLayout());
-        constraints.weightx = 1.0;
-        constraints.weighty = 1.0;
-        constraints.fill = GridBagConstraints.BOTH;
-        int x, y;
-
-        // UNO IMAGE
+        //Initial the JFrame Components.
+        menu = new JPanel();
+        menu.setLayout(null);
         uno_image = new ImageIcon("./UnoCardImages/UnoCard.png");
-        constraints.weightx = 1.0;
-        constraints.gridheight = 3; // span two rows
-        constraints.gridwidth = 2; // span two columns
-        addElement(new JLabel(uno_image), x = 0, y = 0);
-
-        constraints.gridheight = 1; // set row width back to one
-        constraints.gridwidth = 1; // set column width back to one
-
-        // PLAY WITH FRIENDS BUTTON
-        constraints.weightx = 0.5; // make this half the size of the uno image size
-        constraints.anchor = GridBagConstraints.CENTER; // center of space
-        constraints.insets = new Insets(50, 50, 50, 50); // all around padding of 100 pixels 
-        //addElement(new JButton("Play with Friends"), x = 3, y = 0);
-        playWithFriends = new JButton("Play with Friends");
-        playWithFriends.addActionListener(this);
-        addElement(playWithFriends, 3, 0);
-
-        // PLAY WITH BOTS BUTTON
-        addElement(new JButton("Play with Bots"), x = 3, y = 1);
-
-        // QUIT BUTTON
-        //addElement(new JButton("Quit"), x = 3, y = 2);
-        quit = new JButton("Quit");
+        label1 = new JLabel(uno_image);
+        label1.setBounds(50,100,486,759);
+        menu.add(label1);
+        
+        name = new JLabel("Uno");
+        name.setFont(new Font("Arial", Font.BOLD, 40));
+        name.setBounds(620,150, 300,200);
+        menu.add(name);
+        
+        quit = new JButton("Exit");
+        quit.setBounds(700,700,140,100);
+        quit.setBackground(Color.WHITE);
         quit.addActionListener(this);
-        addElement(quit, 3, 2);
-
+        menu.add(quit);
+        
+        playWithPlayer = new JButton("Start with players");
+        playWithPlayer.setBounds(700,400,140,100);
+        playWithPlayer.setBackground(Color.WHITE);
+        playWithPlayer.addActionListener(this);
+        menu.add(playWithPlayer);
+        
+        playWithBots = new JButton("Start with Bots");
+        playWithBots.setBounds(700,550,140,100);
+        playWithBots.setBackground(Color.WHITE);
+        playWithBots.addActionListener(this);
+        menu.add(playWithBots);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(menu);
+        this.pack();
     }
 
-    void addElement(Component component, int x, int y) {
-        constraints.gridx = x;
-        constraints.gridy = y;
-        add(component, constraints);
-    }
-
-    public static void main(String[] args) {
-        f.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosed(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-        f.setSize(1000, 1000);
-        f.setLocation(1000, 1000);
-        f.setContentPane(new StartView());
-        f.setVisible(true);
-    }
-
-    @Override
+    /**
+     * The method that takes cares of the click on different JButtons.
+     */
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == quit) {
             //Directly quit the game.
             System.exit(0);
-        } else if (e.getSource() == playWithFriends) {
+        } else if (e.getSource() == playWithPlayer) {
             //Jumps to the next JFrame.
-            frame1 = new MenuView();
+            JFrame frame1 = new MenuView();
             frame1.setSize(1000,1000);
             frame1.setVisible(true);
-            // TODO FIGURE OUT WHY THIS AUTOMATICALLY CLOSES THE NEXT JFRAME
-            //this.dispose();
-          }
+            this.dispose();
+        } /* else if (e.getSource() == startBots) {
+            //Jumps to the next JFrame.
+            JFrame frame1 = new BotMenuView();
+            frame1.setSize(1000,1000);
+            frame1.setVisible(true);
+            this.dispose();
+        } */
     }
 
-
-
-    
+    /**
+     * Method for starting the UnoGame.
+     */
+    public static void main(String[] args) {
+        JFrame frame = new StartView();
+        frame.setSize(1000,1000);
+        frame.setVisible(true);
+    }
 }
